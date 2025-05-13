4c9f6c1ee48f5703277b703dc7fbd4b106a9765e
ReetamBG <r33t4m@gmail.com>
2025-05-13 13:37:30 +0530

    Added manual input

--- client.cpp
+++ client.cpp
@@ -13,20 +13,22 @@
 #define PORT 8080
 #define HEARTBEAT_INTERVAL 5
 
-std::atomic<bool> found(false);
+using namespace std;
+
+atomic<bool> found(false);
 int password_length;
-std::string charset, target_hash;
+string charset, target_hash;
 
 void send_heartbeat(int sock) {
     while (!found) {
-        std::this_thread::sleep_for(std::chrono::seconds(HEARTBEAT_INTERVAL));
-        std::string heartbeat = "HEARTBEAT\n";
+        this_thread::sleep_for(chrono::seconds(HEARTBEAT_INTERVAL));
+        string heartbeat = "HEARTBEAT\n";
         send(sock, heartbeat.c_str(), heartbeat.size(), 0);
     }
 }
 
-std::string index_to_password(long long index, int length, const std::string& charset) {
-    std::string result(length, charset[0]);
+string index_to_password(long long index, int length, const string& charset) {
+    string result(length, charset[0]);
     int base = charset.size();
     for (long long i = length - 1; i >= 0; --i) {
         result[i] = charset[index % base];
@@ -37,47 +39,47 @@ std::string index_to_password(long long index, int length, const std::string& ch
 
 void process_task(int sock) {
     char buffer[2048];
-    std::string partial_msg;
+    string partial_msg;
 
     while (!found) {
         memset(buffer, 0, sizeof(buffer));
         int valread = recv(sock, buffer, sizeof(buffer), 0);
         if (valread <= 0) break;
 
-        partial_msg += std::string(buffer, valread);
+        partial_msg += string(buffer, valread);
         size_t newline_pos;
-        while ((newline_pos = partial_msg.find('\n')) != std::string::npos) {
-            std::string msg = partial_msg.substr(0, newline_pos);
+        while ((newline_pos = partial_msg.find('\n')) != string::npos) {
+            string msg = partial_msg.substr(0, newline_pos);
             partial_msg.erase(0, newline_pos + 1);
 
             if (msg.find("TASK:") == 0) {
-                std::stringstream ss(msg.substr(5));
+                stringstream ss(msg.substr(5));
                 long long start, end;
-                std::string token;
+                string token;
 
-                std::getline(ss, token, ','); start = std::stoll(token);
-                std::getline(ss, token, ','); end = std::stoll(token);
-                std::getline(ss, token, ','); password_length = std::stoll(token);
-                std::getline(ss, token, ','); target_hash = token;
-                std::getline(ss, token, ','); charset = token;
+                getline(ss, token, ','); start = stoll(token);
+                getline(ss, token, ','); end = stoll(token);
+                getline(ss, token, ','); password_length = stoll(token);
+                getline(ss, token, ','); target_hash = token;
+                getline(ss, token, ','); charset = token;
 
-                std::cout << "[*] Received task: " << start << " to " << end << std::endl;
+                cout << "[*] Received task: " << start << " to " << end << endl;
 
                 for (long long i = start; i <= end && !found; ++i) {
-                    std::string pwd = index_to_password(i, password_length, charset);
-                    std::cout << "[-] Trying: " << pwd << std::endl;
+                    string pwd = index_to_password(i, password_length, charset);
+                    cout << "[-] Trying: " << pwd << endl;
                     if (MD5::hash(pwd) == target_hash) {
-                        std::string found_msg = "FOUND:" + pwd + "\n";
+                        string found_msg = "FOUND:" + pwd + "\n";
                         send(sock, found_msg.c_str(), found_msg.size(), 0);
-                        std::cout << "[+] Found password: " << pwd << std::endl;
+                        cout << "[+] Found password: " << pwd << endl;
                         found = true;
                         break;
                     }
-                    std::this_thread::sleep_for(std::chrono::milliseconds(1)); // Optional
+                    this_thread::sleep_for(chrono::milliseconds(1)); // Optional
                 }
 
                 if (!found) {
-                    std::string done_msg = "DONE\n";
+                    string done_msg = "DONE\n";
                     send(sock, done_msg.c_str(), done_msg.size(), 0);
                 }
             } else if (msg.find("STOP") == 0) {
@@ -92,27 +94,22 @@ int main() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in serv_addr;
 
-    // serv_addr.sin_family = AF_INET;
-    // serv_addr.sin_port = htons(PORT);
-    // inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-    const char* my_IP = "192.168.0.171";
+    const char* my_IP = "192.168.1.48";
     serv_addr.sin_addr.s_addr = inet_addr(my_IP);
 
     if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection failed\n";
+        cerr << "Connection failed\n";
         return 1;
     }
 
-    std::cout << "Connected to server.\n";
+    cout << "Connected to server.\n";
 
-    std::thread hb(send_heartbeat, sock);
+    thread hb(send_heartbeat, sock);
     process_task(sock);
     hb.join();
 
     close(sock);
     return 0;
 }
-
--- server.cpp
+++ server.cpp
@@ -1,4 +1,3 @@
-// server.cpp
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -16,7 +15,10 @@
 #define MAX_CLIENTS 10
 #define CHUNK_SIZE 20000
 
-struct ClientInfo {
+using namespace std;
+
+struct ClientInfo
+{
     int socket;
     int start_index = -1;
     int end_index = -1;
@@ -24,91 +26,121 @@ struct ClientInfo {
     bool active = false;
 };
 
-std::map<int, ClientInfo> clients;
-std::mutex client_mutex;
+map<int, ClientInfo> clients;
+mutex client_mutex;
 
-std::string target_password = "password";
-std::string target_hash = MD5::hash(target_password);
-std::string charset = "abcdefghigklmnopqrtsuvwxyz";
-int password_length = 8;
 long long total_tasks;
 long long next_task_index = 0;
 bool found = false;
-std::string found_password;
+string found_password;
 
-long long totalCombinations(const std::string& charset, int length) {
+long long totalCombinations(const string &charset, int length)
+{
     long long total = 1;
     for (long long i = 0; i < length; i++)
         total *= charset.size();
     return total;
 }
 
-void send_task(int client_socket, long long start_index, long long end_index) {
-    std::string task = "TASK:" + std::to_string(start_index) + "," +
-                       std::to_string(end_index) + "," +
-                       std::to_string(password_length) + "," +
-                       target_hash + "," + charset + "\n";
+void send_task(
+    int client_socket, 
+    long long start_index, 
+    long long end_index, 
+    string target_hash, 
+    int password_length,
+    string charset
+)
+{
+    string task = "TASK:" + to_string(start_index) + "," +
+                  to_string(end_index) + "," +
+                  to_string(password_length) + "," +
+                  target_hash + "," + charset + "\n";
     send(client_socket, task.c_str(), task.size(), 0);
 }
 
-bool assign_task_to_client(int client_id) {
-    if (next_task_index >= total_tasks || found) return false;
+bool assign_task_to_client(int client_id, string target_hash, int password_length, string charset)
+{
+    if (next_task_index >= total_tasks || found)
+        return false;
 
-    ClientInfo& client = clients[client_id];
+    ClientInfo &client = clients[client_id];
     client.start_index = next_task_index;
-    client.end_index = std::min(next_task_index + CHUNK_SIZE - 1, total_tasks - 1);
+    client.end_index = min(next_task_index + CHUNK_SIZE - 1, total_tasks - 1);
     client.last_heartbeat = time(0);
     client.active = true;
 
-    send_task(client.socket, client.start_index, client.end_index);
-    std::cout << "[*] Assigned task to client " << client_id << ": "
-              << client.start_index << " to " << client.end_index << std::endl;
+    send_task(client.socket, client.start_index, client.end_index, target_hash, password_length, charset);
+    cout << "[*] Assigned task to client " << client_id << ": "
+         << client.start_index << " to " << client.end_index << endl;
 
     next_task_index = client.end_index + 1;
     return true;
 }
 
-void handle_client(int client_socket, int client_id) {
+void handle_client(
+    int client_socket, 
+    int client_id, 
+    string target_hash, 
+    int password_length,
+    string charset)
+{
     char buffer[1024];
 
     {
-        std::lock_guard<std::mutex> lock(client_mutex);
-        assign_task_to_client(client_id);
+        lock_guard<mutex> lock(client_mutex);
+        assign_task_to_client(client_id, target_hash, password_length, charset);
     }
 
-    while (!found) {
+    while (!found)
+    {
         memset(buffer, 0, sizeof(buffer));
         int valread = read(client_socket, buffer, sizeof(buffer));
-        if (valread <= 0) break;
+        if (valread <= 0)
+            break;
 
-        std::string msg(buffer);
+        string msg(buffer);
 
-        if (msg.find("HEARTBEAT") != std::string::npos) {
-            std::lock_guard<std::mutex> lock(client_mutex);
+        if (msg.find("HEARTBEAT") != string::npos)
+        {
+            lock_guard<mutex> lock(client_mutex);
             clients[client_id].last_heartbeat = time(0);
-        } else if (msg.find("FOUND:") == 0) {
-            std::string password = msg.substr(6);
+        }
+        else if (msg.find("FOUND:") == 0)
+        {
+            string password = msg.substr(6);
             found = true;
             found_password = password;
-            std::cout << "\nPassword found by client " << client_id << ": " << password << std::endl;
+            cout << "\nPassword found by client " << client_id << ": " << password << endl;
 
-            std::lock_guard<std::mutex> lock(client_mutex);
-            for (auto& [id, info] : clients) {
-                std::string stop_msg = "STOP\n";
+            lock_guard<mutex> lock(client_mutex);
+            for (auto &[id, info] : clients)
+            {
+                string stop_msg = "STOP\n";
                 send(info.socket, stop_msg.c_str(), stop_msg.size(), 0);
             }
             return;
-        } else if (msg.find("DONE") == 0) {
-            std::lock_guard<std::mutex> lock(client_mutex);
+        }
+        else if (msg.find("DONE") == 0)
+        {
+            lock_guard<mutex> lock(client_mutex);
             clients[client_id].active = false;
 
             // Assign new task only if available
-            assign_task_to_client(client_id);
+            assign_task_to_client(client_id, target_hash, password_length, charset);
         }
     }
 }
 
-int main() {
+int main()
+{
+    // variable declaration
+    string target_password;
+    cout << "Enter password to crack: ";
+    cin >> target_password;
+    string target_hash = MD5::hash(target_password);
+    string charset = "abcdefghijklmnopqrstuvwxyz";  // Fixed typo in charset
+    int password_length = target_password.length();
+
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
@@ -117,39 +149,36 @@ int main() {
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    // address.sin_family = AF_INET;
-    // address.sin_addr.s_addr = INADDR_ANY;
-    // address.sin_port = htons(PORT);
-
     address.sin_family = AF_INET;
     address.sin_port = htons(PORT);
-    const char* my_IP = "192.168.0.171";
+    const char *my_IP = "192.168.1.48";
     address.sin_addr.s_addr = inet_addr(my_IP);
 
     bind(server_fd, (struct sockaddr *)&address, sizeof(address));
     listen(server_fd, MAX_CLIENTS);
 
-    std::cout << "[*] Server started on port " << PORT << "\n";
+    cout << "[*] Server started on port " << PORT << "\n";
     total_tasks = totalCombinations(charset, password_length);
 
     int client_id = 0;
-    while (!found) {
+    while (!found)
+    {
         new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-        if (new_socket < 0) continue;
+        if (new_socket < 0)
+            continue;
 
         {
-            std::lock_guard<std::mutex> lock(client_mutex);
+            lock_guard<mutex> lock(client_mutex);
             clients[client_id] = {new_socket};
         }
 
-        std::thread t(handle_client, new_socket, client_id);
+        thread t(handle_client, new_socket, client_id, target_hash, password_length, charset);
         t.detach();
 
-        std::cout << "[+] Client " << client_id << " connected.\n";
+        cout << "[+] Client " << client_id << " connected.\n";
         client_id++;
     }
 
     close(server_fd);
     return 0;
 }
-
